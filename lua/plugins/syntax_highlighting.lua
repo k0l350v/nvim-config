07cb558867cf0902e688d1f193fe16618751671d
Ivan Kolesov <k0l350v1@gmail.com>
2023-01-25 18:50:22 +0100

    add syntax highlighting with treesitter

---
+++ lua/plugins/syntax_highlighting.lua
@@ -0,0 +1,53 @@
+local M = {
+	'nvim-treesitter/nvim-treesitter',
+	version = false,
+    build = ":TSUpdate",
+    event = "BufReadPost",
+	opts = {
+		highlight = { enable = true },
+		ensure_installed = {
+			'bash',
+			'c',
+			'c_sharp',
+			'cmake',
+			'cpp',
+			'diff',
+			'fish',
+			'gitignore',
+			'go',
+			'help',
+			'html',
+			'http',
+			'java',
+			'javascript',
+			'json',
+			'jsdoc',
+			'jsonc',
+			'kotlin',
+			'latex',
+			'llvm',
+			'lua',
+			'markdown',
+			'markdown_inline',
+			'ninja',
+			'pascal',
+			'python',
+			'query',
+			'regex',
+			'ruby',
+			'rust',
+			'scala',
+			'sql',
+			'swift',
+			'sxhkdrc',
+			'vim',
+			'yaml',
+		}
+	}
+}
+
+function M.config(_, opts)
+	require('nvim-treesitter.configs').setup(opts)
+end
+
+return M
