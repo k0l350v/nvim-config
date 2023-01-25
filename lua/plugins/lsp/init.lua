4d20388786bc1a1eba259b5153dac601c8c6aec7
Ivan Kolesov <k0l350v1@gmail.com>
2023-01-25 17:55:07 +0100

    setup lsp servers

---
+++ lua/plugins/lsp/init.lua
@@ -0,0 +1,40 @@
+local M = {
+	'neovim/nvim-lspconfig',
+	event = 'BufReadPre',
+	dependencies = {
+		{ 'folke/neodev.nvim', opts = { experimental = { pathStrict = true } } },
+
+		'williamboman/mason.nvim',
+		'williamboman/mason-lspconfig.nvim',
+	}
+}
+
+local ensure_installed_servers = {
+	'sumneko_lua',
+	'clangd',
+}
+
+local function get_server_capabilities()
+	local status_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+	if status_ok then
+		return cmp_nvim_lsp.default_capabilities()
+	end
+
+	return vim.lsp.protocol.make_client_capabilities()
+end
+
+local function server_setup_handler(server)
+	local has_custom_opts, custom_opts = pcall(require, 'plugins/lsp/configs/' .. server)
+	local server_opts = has_custom_opts and custom_opts or {}
+	server_opts.capabilities = get_server_capabilities()
+
+	require('lspconfig')[server].setup(server_opts)
+end
+
+function M.config()
+	local mlsp = require('mason-lspconfig')
+	mlsp.setup({ ensure_installed = ensure_installed_servers })
+	mlsp.setup_handlers({ server_setup_handler })
+end
+
+return M
---
+++ lua/plugins/tools_manager.lua
@@ -0,0 +1,7 @@
+local M = {
+	'williamboman/mason.nvim',
+	cmd = 'Mason',
+	opts = {},
+}
+
+return M
