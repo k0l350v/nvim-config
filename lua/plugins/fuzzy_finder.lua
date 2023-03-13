c7d0594e097fe017a993cce0116e64eeda80ec05
Ivan Kolesov <k0l350v1@gmail.com>
2023-03-13 22:36:27 +0100

    add telescope mappings

--- lua/plugins/fuzzy_finder.lua
+++ lua/plugins/fuzzy_finder.lua
@@ -12,14 +12,23 @@ local M = {
 			end
 		},
 	},
+	keys = {
+		{ '<leader>fr', '<cmd>Telescope resume<cr>', desc = 'Find resume' },
+		{ '<leader>fR', '<cmd>Telescope oldfiles<cr>', desc = 'Find recent files' },
+		{ '<leader>ff', '<cmd>Telescope find_files hidden=true<cr>', desc = 'Find files' },
+		{ '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Find buffers' },
+		{ '<leader>fw', 'Telescope grep_string hidden=true', desc = 'Find word from cursor' },
+		{ '<leader>:', '<cmd>Telescope command_history<cr>', desc = 'Find in command history' },
+		{ '<leader>/', '<cmd>Telescope live_grep hidden=true<cr>', desc = 'Find string (Grep)' },
+	},
 	opts = {
 		defaults = {
 			layout_strategy = 'horizontal',
 			layout_config = { prompt_position = 'top' },
 			sorting_strategy = 'ascending',
 			winblend = 0,
-			prompt_prefix = " ",
-			selection_caret = " ",
+			prompt_prefix = ' ',
+			selection_caret = ' ',
 			mappings = {
 				n = {
 					['q'] = function(...)
