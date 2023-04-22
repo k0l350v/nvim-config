b8e68fc98bef70bdebd9655b5c2319d8122b8c02
Ivan Kolesov <k0l350v1@gmail.com>
2023-04-22 20:05:39 +0200

    add keybinding for buffers and tabs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,8 +1,9 @@
 local M = {
 	'akinsho/bufferline.nvim',
 	dependencies = {
-		'catppuccin',
-		{ 'tiagovla/scope.nvim', opts = {} }
+		{ 'catppuccin' },
+		{ 'tiagovla/scope.nvim', opts = {} },
+		{ 'famiu/bufdelete.nvim' }
 	},
 	event = 'VeryLazy',
 	opts = {
@@ -18,7 +19,29 @@ local M = {
 				}
 			}
 		}
-	}
+	},
+	keys = {
+		{ '<C-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next buffer' },
+		{ '<C-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Previous buffer' },
+		{ '<leader><C-l>', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer right' },
+		{ '<leader><C-h>', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer left' },
+
+		{ '<leader><tab>l', function() vim.cmd.tabnext() end, desc = 'Next tab' },
+		{ '<leader><tab>h', function() vim.cmd.tabprevious() end, desc = 'Previous tab' },
+		{ '<leader><tab>n', function() vim.cmd.tabnew() end, desc = 'New tab' },
+		{ '<leader><tab>c', function() vim.cmd.tabclose() end, desc = 'Delete tab' },
+
+		{ '<C-w>d', function() require('bufdelete').bufdelete(0, true) end, desc = 'Close buffer' },
+		{ '<leader>bp', '<cmd>BufferLineTogglePin<cr>', desc = 'Pin buffer' },
+		{ '<leader>bs', '<cmd>BufferLinePick<cr>', desc = 'Select buffer from bufferline' },
+		{ '<leader>bS', '<cmd>BufferLinePickClose<cr>', desc = 'Close selected buffer from bufferline' },
+		{ '<leader>bh', '<cmd>BufferLineCloseLeft<cr>', desc = 'Close all buffers to the left' },
+		{ '<leader>bl', '<cmd>BufferLineCloseRight<cr>', desc = 'Close all buffers to the right' },
+		{ '<leader>bo'
+			, '<cmd>BufferLineCloseLeft<cr><cmd>BufferLineCloseRight<cr>'
+			, desc = 'Close other buffers'
+		},
+	},
 }
 
 function M.config(_, opts)
