2a8498147ceb3bea93a3d533416564ebbf2e2e3c
Ivan Kolesov <k0l350v1@gmail.com>
2023-04-23 23:58:13 +0200

    add diff view plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,31 +1,36 @@
 local M = {
-	'lewis6991/gitsigns.nvim',
-	event = { 'BufReadPre', 'BufNewFile' },
-	opts = {
-		current_line_blame = true,
-		on_attach = function(buffer)
-			local gs = package.loaded.gitsigns
+	{
+		'lewis6991/gitsigns.nvim',
+		event = { 'BufReadPre', 'BufNewFile' },
+		opts = {
+			current_line_blame = true,
+			on_attach = function(buffer)
+				local gs = package.loaded.gitsigns
 
-			local function map(mode, l, r, desc)
-				vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+				local function map(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+				end
+
+				map('n', ']h', gs.next_hunk, 'Next hunk')
+				map('n', '[h', gs.prev_hunk, 'Previous hunk')
+
+				map({ 'n', 'v' }, '<leader>gs', ':Gitsigns stage_hunk<CR>', 'Stage hunk')
+				map({ 'n', 'v' }, '<leader>gr', ':Gitsigns reset_hunk<CR>', 'Reset hunk')
+				map('n', '<leader>gS', gs.stage_buffer, 'Stage buffer')
+				map('n', '<leader>gR', gs.reset_buffer, 'Reset buffer')
+				map('n', '<leader>gu', gs.undo_stage_hunk, 'Undo stage hunk')
+				map('n', '<leader>gh', gs.preview_hunk, 'Hunk preview')
+
+				map('n', '<leader>gb', function() gs.blame_line({ full = true }) end, 'Blame Line')
 			end
+		}
+	},
 
-			map('n', ']h', gs.next_hunk, 'Next hunk')
-			map('n', '[h', gs.prev_hunk, 'Previous hunk')
-
-			map({ 'n', 'v' }, '<leader>gs', ':Gitsigns stage_hunk<CR>', 'Stage hunk')
-			map({ 'n', 'v' }, '<leader>gr', ':Gitsigns reset_hunk<CR>', 'Reset hunk')
-			map('n', '<leader>gS', gs.stage_buffer, 'Stage buffer')
-			map('n', '<leader>gR', gs.reset_buffer, 'Reset buffer')
-			map('n', '<leader>gu', gs.undo_stage_hunk, 'Undo stage hunk')
-			map('n', '<leader>gh', gs.preview_hunk, 'Hunk preview')
-
-			map('n', '<leader>gb', function() gs.blame_line({ full = true }) end, 'Blame Line')
-
-			map('n', '<leader>gd', gs.diffthis, 'Diff this')
-			map('n', '<leader>gD', function() gs.diffthis('~') end, 'Diff this ~')
-		end
-	}
+	{
+		'sindrets/diffview.nvim',
+		cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles' },
+		keys = { { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'DiffView' } },
+	},
 }
 
 return M
