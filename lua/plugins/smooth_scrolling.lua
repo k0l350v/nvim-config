1f310cc88cfdc441cad883ba5049848e8ea1c885
Ivan Kolesov <k0l350v1@gmail.com>
2023-04-30 17:57:23 +0200

    smooth scrolling plugin

---
+++ lua/plugins/smooth_scrolling.lua
@@ -0,0 +1,14 @@
+local M = {
+	'karb94/neoscroll.nvim',
+	keys = {
+			'<C-u>', '<C-d>',
+			'<C-b>', '<C-f>',
+			'<C-y>', '<C-e>',
+			'zt', 'zz', 'zb'
+	},
+	opts = {
+		easing_function = 'quadratic'
+	}
+}
+
+return M
