3188b80730e920648e9536af73f6e4378f1405d8
Ivan Kolesov <k0l350v1@gmail.com>
2023-05-08 11:33:30 +0200

    animate vim actions

---
+++ lua/plugins/ui_animations.lua
@@ -0,0 +1,20 @@
+local M = {
+	'echasnovski/mini.animate',
+	event = 'VeryLazy',
+	opts = function ()
+		local animate = require('mini.animate')
+		return {
+			resize = {
+				timing = animate.gen_timing.linear({ duration = 100, unit = 'total' })
+			},
+			scroll = { enable = false },
+			open = { enable = false },
+			close = { enable = false },
+		}
+	end,
+	config = function (_, opts)
+		require('mini.animate').setup(opts)
+	end
+}
+
+return M
