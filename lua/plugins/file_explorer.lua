1770ec9845611ccb3130d9166193cf814aeea62d
Ivan Kolesov <k0l350v1@gmail.com>
2023-09-14 22:55:40 +0200

    add simple file manager for fast movement around current file

--- lua/plugins/file_explorer.lua
+++ lua/plugins/file_explorer.lua
@@ -1,30 +1,54 @@
 local M = {
-	'nvim-neo-tree/neo-tree.nvim',
-	dependencies = {
-		'nvim-tree/nvim-web-devicons',
-		'MunifTanjim/nui.nvim',
-		'nvim-lua/plenary.nvim'
-	},
-	cmd = 'Neotree',
-	keys = {
-		{
-			'<leader>fe',
-			function()
-				require('neo-tree.command').execute({ toggle = true, dir = vim.loop.cwd() })
-			end,
-			desc = 'Toggle File Explorer (cwd)',
-		}
-	},
-	init = function()
-		vim.g.neo_tree_remove_legacy_commands = 1
-		if vim.fn.argc() == 1 then
-			local stat = vim.loop.fs_stat(vim.fn.argv(0))
-			if stat and stat.type == 'directory' then
-				require('neo-tree')
+	{
+		'nvim-neo-tree/neo-tree.nvim',
+		dependencies = {
+			'nvim-tree/nvim-web-devicons',
+			'MunifTanjim/nui.nvim',
+			'nvim-lua/plenary.nvim'
+		},
+		cmd = 'Neotree',
+		keys = {
+			{
+				'<leader>fe',
+				function()
+					require('neo-tree.command').execute({ toggle = true, dir = vim.loop.cwd() })
+				end,
+				desc = 'Toggle File Explorer window(cwd)',
+			}
+		},
+		init = function()
+			vim.g.neo_tree_remove_legacy_commands = 1
+			if vim.fn.argc() == 1 then
+				local stat = vim.loop.fs_stat(vim.fn.argv(0))
+				if stat and stat.type == 'directory' then
+					require('neo-tree')
+				end
 			end
-		end
-	end,
-	opts = {}
+		end,
+		opts = {}
+	},
+	{
+		'echasnovski/mini.files',
+		opts = {
+			windows = {
+				preview = true,
+				width_focus = 50,
+				width_preview = 50,
+			},
+			optioons = {
+				use_as_default_explorer = false,
+			}
+		},
+		keys = {
+			{
+				'<leader>fm',
+				function()
+					require('mini.files').open(vim.api.nvim_buf_get_name(0), true)
+				end,
+				desc = 'Open File Manager (current file directory)',
+			}
+		},
+	}
 }
 
 return M
