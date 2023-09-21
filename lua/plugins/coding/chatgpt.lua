fab0dc545a0a005190eef10ccb7b881d889ca85b
Ivan Kolesov <k0l350v1@gmail.com>
2023-09-21 06:27:52 +0200

    added chatGPT plugin

---
+++ lua/plugins/coding/chatgpt.lua
@@ -0,0 +1,12 @@
+local M = {
+	'jackMort/ChatGPT.nvim',
+	dependencies = {
+		'MunifTanjim/nui.nvim',
+		'nvim-lua/plenary.nvim',
+		'nvim-telescope/telescope.nvim'
+	},
+	cmd = { 'ChatGPTActAs', 'ChatGPT'},
+	opts = {},
+}
+
+return M
