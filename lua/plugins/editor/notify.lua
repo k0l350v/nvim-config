8e3f8aca322e65cd6d8e6e1b37012f3b7d97e221
Ivan Kolesov <k0l350v1@gmail.com>
2024-01-12 22:20:47 +0100

    add notifications ui windows

---
+++ lua/plugins/editor/notify.lua
@@ -0,0 +1,24 @@
+local M = {
+	'rcarriga/nvim-notify',
+	event = 'VeryLazy',
+	opts = {
+		timeout = 5000,
+		max_height = function()
+			return math.floor(vim.o.lines * 0.75)
+		end,
+		max_width = function()
+			return math.floor(vim.o.columns * 0.75)
+		end,
+		on_open = function(win)
+			vim.api.nvim_win_set_config(win, { zindex = 100 })
+		end,
+		background_colour = '#000000',
+		fps = 120,
+		stages = 'fade_in_slide_out',
+	},
+	init = function ()
+		vim.notify = require('notify')
+	end
+}
+
+return M
