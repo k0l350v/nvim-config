32fffe3e54c37cfc2b914d7bacbde6805c515a47
Ivan Kolesov <k0l350v1@gmail.com>
2024-01-12 16:59:46 +0100

    fix indentation highlighting

--- lua/plugins/coding/indentation_highlighting.lua
+++ lua/plugins/coding/indentation_highlighting.lua
@@ -1,19 +1,32 @@
 local M = {
-	'lukas-reineke/indent-blankline.nvim',
-	event = 'BufReadPost',
-	opts = {
-		show_current_context = true,
-		use_treesitter = true,
+	{
+		'lukas-reineke/indent-blankline.nvim',
+		event = 'BufReadPost',
+		opts = {
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+			scope = { enabled = false },
+		},
+		config = function(_, opts)
+			vim.opt.list = true
+			vim.opt.listchars:append('space:·')
+			vim.opt.listchars:append('eol:↴')
+			vim.opt.listchars:append('tab:-->')
+
+			require('ibl').setup(opts)
+		end
+	},
+	{
+		'echasnovski/mini.indentscope',
+		version = false,
+		event = 'BufReadPost',
+		opts = {
+			symbol = '│',
+			options = { try_as_border = true },
+		},
 	}
 }
-
-function M.config(_, opts)
-	vim.opt.list = true
-	vim.opt.listchars:append('space:·')
-	vim.opt.listchars:append('eol:↴')
-	vim.opt.listchars:append('tab:-->')
-
-	require('indent_blankline').setup(opts)
-end
 
 return M
