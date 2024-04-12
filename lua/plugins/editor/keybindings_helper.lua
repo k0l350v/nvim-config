4312b5444ababe065d1cb9c35c2052ace4293d2f
Ivan Kolesov <k0l350v1@gmail.com>
2024-04-13 00:26:35 +0200

    catppuccin which-key integration

--- lua/plugins/editor/keybindings_helper.lua
+++ lua/plugins/editor/keybindings_helper.lua
@@ -1,7 +1,17 @@
 local M = {
-	'folke/which-key.nvim',
-	event = 'VeryLazy',
-	opts = {}
+	{
+		'folke/which-key.nvim',
+		event = 'VeryLazy',
+		opts = {},
+	},
+	{
+		'catppuccin',
+		opts = {
+			integrations = {
+				which_key = true,
+			},
+		},
+	},
 }
 
 return M
