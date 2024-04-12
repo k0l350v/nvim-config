11e7b683365d6b9f161aeb1b716c0a30db8a3017
Ivan Kolesov <k0l350v1@gmail.com>
2024-04-13 00:24:04 +0200

    catppuccin notify integration

--- lua/plugins/editor/notify.lua
+++ lua/plugins/editor/notify.lua
@@ -1,24 +1,34 @@
 local M = {
-	'rcarriga/nvim-notify',
-	event = 'VeryLazy',
-	opts = {
-		timeout = 5000,
-		max_height = function()
-			return math.floor(vim.o.lines * 0.75)
+	{
+		'rcarriga/nvim-notify',
+		event = 'VeryLazy',
+		opts = {
+			timeout = 5000,
+			max_height = function()
+				return math.floor(vim.o.lines * 0.75)
+			end,
+			max_width = function()
+				return math.floor(vim.o.columns * 0.75)
+			end,
+			on_open = function(win)
+				vim.api.nvim_win_set_config(win, { zindex = 100 })
+			end,
+			background_colour = '#000000',
+			fps = 120,
+			stages = 'fade_in_slide_out',
+		},
+		init = function()
+			vim.notify = require('notify')
 		end,
-		max_width = function()
-			return math.floor(vim.o.columns * 0.75)
-		end,
-		on_open = function(win)
-			vim.api.nvim_win_set_config(win, { zindex = 100 })
-		end,
-		background_colour = '#000000',
-		fps = 120,
-		stages = 'fade_in_slide_out',
 	},
-	init = function ()
-		vim.notify = require('notify')
-	end
+	{
+		'catppuccin',
+		opts = {
+			integrations = {
+				notify = true,
+			},
+		},
+	},
 }
 
 return M
