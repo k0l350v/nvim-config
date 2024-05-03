c6894e5c8430b4806d104db921e1160ba16b1311
Ivan Kolesov <k0l350v1@gmail.com>
2024-05-03 22:07:26 +0200

    refactor greeter

--- lua/plugins/editor/greeter.lua
+++ lua/plugins/editor/greeter.lua
@@ -1,16 +1,35 @@
 local M = {
-	'goolord/alpha-nvim',
-	dependecies = { 'nvim-tree/nvim-web-devicons' },
+	'nvimdev/dashboard-nvim',
 	event = 'VimEnter',
+	opts = function()
+		local opts = {
+			theme = 'doom',
+			config = {
+				center = {
+					{ action = 'ene | startinsert', desc = 'New file', icon = ' ', key = 'e' },
+					{ action = 'qa', desc = 'Quit', icon = ' ', key = 'q' },
+				},
+				footer = function()
+					local stats = require('lazy').stats()
+					local ms = math.floor((stats.startuptime * 100 + 0.5) / 100)
+					return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
+				end,
+			},
+		}
+
+		-- close Lazy and re-open when the dashboard is ready
+		if vim.o.filetype == 'lazy' then
+			vim.cmd.close()
+			vim.api.nvim_create_autocmd('User', {
+				pattern = 'DashboardLoaded',
+				callback = function()
+					require('lazy').show()
+				end,
+			})
+		end
+
+		return opts
+	end,
 }
-
-function M.opts()
-	local dashboard = require("alpha.themes.dashboard")
-	return dashboard
-end
-
-function M.config(_, dashboard)
-	require('alpha').setup(dashboard.opts)
-end
 
 return M
