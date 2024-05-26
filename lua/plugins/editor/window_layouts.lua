6e9daeb388696787a110b78d830ecfb009dcc6ed
Ivan Kolesov <k0l350v1@gmail.com>
2024-05-26 19:14:58 +0200

    Added window layouts managment

---
+++ lua/plugins/editor/window_layouts.lua
@@ -0,0 +1,16 @@
+local M = {
+	{
+		'folke/edgy.nvim',
+		event = 'VeryLazy',
+		opts = {
+			animate = {
+				enabled = false,
+			},
+			wo = {
+				winbar = false,
+			},
+		},
+	},
+}
+
+return M
