dacfa5f017d1e11085f413643008ad2957560c45
Ivan Kolesov <k0l350v1@gmail.com>
2024-05-26 19:18:16 +0200

    Move overseer to left layout

--- lua/plugins/coding/task_manager.lua
+++ lua/plugins/coding/task_manager.lua
@@ -1,34 +1,46 @@
 local M = {
-	'stevearc/overseer.nvim',
-	keys = {
-		{ '<leader>tt', '<cmd>OverseerToggle<cr>', desc = 'Toggle Task Window' },
-		{ '<leader>tq', '<cmd>OverseerQuickAction<cr>', desc = 'Last Task Quick Action' },
-	},
-	opts = {
-		task_list = {
-			direction = 'bottom',
-			max_width = 0.4,
-			max_height = 0.6,
-			default_detail = 1,
+	{
+		'stevearc/overseer.nvim',
+		keys = {
+			{ '<leader>tt', '<cmd>OverseerToggle<cr>', desc = 'Toggle Task Window' },
+			{ '<leader>tq', '<cmd>OverseerQuickAction<cr>', desc = 'Last Task Quick Action' },
 		},
-		component_aliases = {
-			default = {
-				{ 'display_duration', detail_level = 1 },
-				{ 'on_complete_dispose', timeout = 900 },
-				'on_exit_set_status',
+		opts = {
+			task_list = {
+				direction = 'left',
+			},
+			component_aliases = {
+				default = {
+					{ 'display_duration', detail_level = 1 },
+					{ 'on_complete_dispose', timeout = 900 },
+					'on_exit_set_status',
+				},
 			},
 		},
+		config = function(_, opts)
+			local problem_matcher = require('overseer.template.vscode.problem_matcher')
+
+			opts.problem_matchers = opts.problem_matchers or {}
+			for name, matcher in pairs(opts.problem_matchers) do
+				problem_matcher.register_problem_matcher(name, matcher)
+			end
+
+			require('overseer').setup(opts)
+		end,
 	},
-	config = function(_, opts)
-		local problem_matcher = require('overseer.template.vscode.problem_matcher')
-
-		opts.problem_matchers = opts.problem_matchers or {}
-		for name, matcher in pairs(opts.problem_matchers) do
-			problem_matcher.register_problem_matcher(name, matcher)
-		end
-
-		require('overseer').setup(opts)
-	end,
+	{
+		'folke/edgy.nvim',
+		optional = true,
+		opts = function(_, opts)
+			opts.left = opts.left or {}
+			table.insert(opts.left, {
+				title = 'Overseer',
+				ft = 'OverseerList',
+				open = 'OverseerToggle!',
+				size = { width = 0.15 },
+			})
+		end,
+	},
 }
 
 return M
