0b0c87c7636cad2816d314e3196ff8acdf2ff401
Ivan Kolesov <k0l350v1@gmail.com>
2024-05-26 19:29:06 +0200

    layouts for trouble plugin

--- lua/plugins/coding/diagnostics.lua
+++ lua/plugins/coding/diagnostics.lua
@@ -1,12 +1,33 @@
 local M = {
-	'folke/trouble.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	branch = 'dev',
-	keys = {
-		{ '<leader>xt', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
-		{ '<leader>xT', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
+	{
+		'folke/trouble.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		branch = 'dev',
+		keys = {
+			{ '<leader>xt', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
+			{ '<leader>xT', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
+		},
+		opts = {},
 	},
-	opts = {},
+	{
+		'folke/edgy.nvim',
+		optional = true,
+		opts = function(_, opts)
+			for _, pos in ipairs({ 'top', 'bottom', 'left', 'right' }) do
+				opts[pos] = opts[pos] or {}
+				table.insert(opts[pos], {
+					ft = 'trouble',
+					filter = function(_, win)
+						return vim.w[win].trouble
+							and vim.w[win].trouble.position == pos
+							and vim.w[win].trouble.type == 'split'
+							and vim.w[win].trouble.relative == 'editor'
+							and not vim.w[win].trouble_preview
+					end,
+				})
+			end
+		end,
+	},
 }
 
 return M
