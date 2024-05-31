0ae4cb5a17d055ce986f323efb4f1a775a89694e
Ivan Kolesov <k0l350v1@gmail.com>
2024-05-31 19:19:59 +0200

    trouble main

--- lua/plugins/coding/diagnostics.lua
+++ lua/plugins/coding/diagnostics.lua
@@ -2,7 +2,6 @@ local M = {
 	{
 		'folke/trouble.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
-		branch = 'dev',
 		keys = {
 			{ '<leader>xt', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
 			{ '<leader>xT', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
