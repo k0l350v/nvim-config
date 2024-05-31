2dd713bc349d9aa4d72bc5ff3f50279c56ce3c2e
Ivan Kolesov <k0l350v1@gmail.com>
2024-05-31 18:50:37 +0200

    add float statusline

--- lua/plugins/editor/statusline.lua
+++ lua/plugins/editor/statusline.lua
@@ -1,11 +1,39 @@
 local M = {
-	'nvim-lualine/lualine.nvim',
-	event = 'VeryLazy',
-	opts = {
-		options = {
-			theme = 'catppuccin',
+	{
+		'nvim-lualine/lualine.nvim',
+		event = 'VeryLazy',
+		opts = {
+			options = {
+				theme = 'catppuccin',
+			},
 		},
-	}
+	},
+	{
+		'b0o/incline.nvim',
+		event = 'BufEnter',
+		dependencies = {
+			'nvim-tree/nvim-web-devicons',
+		},
+		opts = {
+			render = function(props)
+				local unfocused = 'NonText'
+				local focused = 'Identifier'
+				local color = props.focused and focused or unfocused
+
+				local filename = vim.fn.fnamemodify(vim.fn.bufname(props.buf), ':t')
+
+				local filetype_icon, filetype_color = require('nvim-web-devicons').get_icon_color(filename)
+				local icon = props.focused and { filetype_icon, guifg = filetype_color }
+					or { filetype_icon, group = unfocused }
+
+				return {
+					{ icon },
+					{ filetype_icon and ' ' or '' },
+					{ filename, group = color },
+				}
+			end,
+		},
+	},
 }
 
 return M
