06313cbb79dc3c2159f82a9a917a4f761c7fd645
Ivan Kolesov <k0l350v1@gmail.com>
2024-06-05 08:09:20 +0200

    fix telesope window selection with edgy

--- lua/plugins/editor/window_layouts.lua
+++ lua/plugins/editor/window_layouts.lua
@@ -11,6 +11,18 @@ local M = {
 			},
 		},
 	},
+	{
+		'nvim-telescope/telescope.nvim',
+		optional = true,
+		opts = {
+			defaults = {
+				get_selection_window = function()
+					require('edgy').goto_main()
+					return 0
+				end,
+			},
+		},
+	},
 }
 
 return M
