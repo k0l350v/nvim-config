31f0b5a6fcea78804af3653fc65106b43aadef5e
klv <k0l350v1@gmail.com>
2025-03-08 06:27:19 +0100

    replace neop tree with snacks explorer

--- lua/plugins/editor/file_explorer.lua
+++ lua/plugins/editor/file_explorer.lua
@@ -1,77 +1,16 @@
 local M = {
-	{
-		'nvim-neo-tree/neo-tree.nvim',
-		dependencies = {
-			'nvim-tree/nvim-web-devicons',
-			'nui.nvim',
-			'nvim-lua/plenary.nvim',
+	desc = 'Snacks File Explorer',
+	recommended = true,
+	'folke/snacks.nvim',
+	opts = { explorer = {} },
+	keys = {
+		{
+			'<leader>e',
+			function()
+				Snacks.explorer()
+			end,
+			desc = 'Explorer Snacks (cwd)',
 		},
-		cmd = 'Neotree',
-		keys = {
-			{
-				'<leader>fe',
-				function()
-					require('neo-tree.command').execute({ toggle = true, dir = vim.loop.cwd() })
-				end,
-				desc = 'Toggle File Explorer window(cwd)',
-			},
-		},
-		init = function()
-			vim.api.nvim_create_autocmd('BufEnter', {
-				group = vim.api.nvim_create_augroup('Neotree_start_directory', { clear = true }),
-				desc = 'Start Neo-tree with directory',
-				once = true,
-				callback = function()
-					if package.loaded['neo-tree'] then
-						return
-					else
-						---@diagnostic disable-next-line: param-type-mismatch
-						local stats = vim.uv.fs_stat(vim.fn.argv(0))
-						if stats and stats.type == 'directory' then
-							require('neo-tree')
-						end
-					end
-				end,
-			})
-		end,
-		opts = {
-			enable_diagnostics = false,
-			enable_git_status = false,
-			sources = { 'filesystem' },
-			filesystem = {
-				follow_current_file = { enabled = true },
-			},
-			default_component_configs = {
-				indent = {
-					with_expanders = true,
-					expander_collapsed = '',
-					expander_expanded = '',
-					indent_marker = '│',
-					last_indent_marker = '╰',
-				},
-				icon = {
-					folder_closed = ' ',
-					folder_open = ' ',
-					folder_empty = ' ',
-				},
-				modified = {
-					symbol = ' ',
-				},
-			},
-		},
-	},
-	{
-		'folke/edgy.nvim',
-		optional = true,
-		ft = 'neo-tree',
-		opts = function(_, opts)
-			opts.left = vim.list_extend(opts.left or {}, {
-				{
-					title = 'Neo-Tree',
-					ft = 'neo-tree',
-				},
-			})
-		end,
 	},
 }
 
