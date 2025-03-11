d939817f98f3c76cc0a327c6fc29f618e441cc6b
klv <k0l350v1@gmail.com>
2025-03-11 19:45:47 +0100

    add lualine trouble section

--- lua/plugins/coding/diagnostics.lua
+++ lua/plugins/coding/diagnostics.lua
@@ -27,6 +27,28 @@ local M = {
 			end
 		end,
 	},
+	{
+		'lualine.nvim',
+		opts = function(_, opts)
+			local trouble = require('trouble')
+			local symbols = trouble.statusline({
+				mode = 'lsp_document_symbols',
+				groups = {},
+				title = false,
+				filter = { range = true },
+				format = '{kind_icon}{symbol.name:Normal}',
+				-- The following line is needed to fix the background color
+				-- Set it to the lualine section you want to use
+				hl_group = 'lualine_c_normal',
+			})
+
+			opts.sections = vim.tbl_deep_extend('force', opts.sections or {}, { lualine_c = {} })
+			table.insert(opts.sections.lualine_c, {
+				symbols.get,
+				cond = symbols.has,
+			})
+		end,
+	},
 }
 
 return M
