27fe99aa6886c8cced16b556943c15401f574705
klv <k0l350v1@gmail.com>
2025-06-04 14:40:44 +0200

    refactor(snippets): remove keybindings for snippet navigation
    Remove hotkeys for jumping between snippet positions in LuaSnip.
    This avoids conflicts with other plugins that use the Tab key.

--- lua/plugins/coding/snippets.lua
+++ lua/plugins/coding/snippets.lua
@@ -5,30 +5,6 @@ local M = {
 			'rafamadriz/friendly-snippets',
 		},
 		opts = {},
-		keys = {
-			{
-				'<Tab>',
-				function()
-					return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
-				end,
-				expr = true,
-				mode = 'i',
-			},
-			{
-				'<Tab>',
-				function()
-					require('luasnip').jump(1)
-				end,
-				mode = 's',
-			},
-			{
-				'<S-Tab>',
-				function()
-					require('luasnip').jump(-1)
-				end,
-				mode = { 'i', 's' },
-			},
-		},
 	},
 	{
 		'saghen/blink.cmp',
