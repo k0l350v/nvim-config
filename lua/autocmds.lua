6623958f70add0f4a2a2357640502f0983031300
klv <k0l350v1@gmail.com>
2025-07-07 15:30:45 +0200

    feat(config): add autosave autocmd for automatic file saving

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local augroup = vim.api.nvim_create_augroup('AutoReload', { clear = true })
+local autosave_augroup = vim.api.nvim_create_augroup('AutoSave', { clear = true })
 
 function M.setup()
 	vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave', 'CursorHold', 'CursorHoldI' }, {
@@ -9,6 +10,16 @@ function M.setup()
 		callback = function()
 			if vim.o.buftype ~= 'nofile' then
 				vim.cmd('checktime')
+			end
+		end,
+	})
+
+	vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
+		group = autosave_augroup,
+		desc = 'Auto-save buffer when leaving insert mode, after text changes',
+		callback = function()
+			if vim.bo.modified and vim.bo.buftype == '' and vim.fn.expand('%') ~= '' and not vim.bo.readonly then
+				vim.cmd('silent! write')
 			end
 		end,
 	})
